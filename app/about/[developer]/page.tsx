33f0cc09fffc2279cd7a9e143176e4a74cbd020b
suchi <suchibankoti95@gmail.com>
2023-11-04 13:01:36 +0530

    dynamic routes by value

--- app/about/[developer]/page.tsx
+++ app/about/[developer]/page.tsx
@@ -1,7 +1,14 @@
-export default function Developer({ params }: { params: { developer: string } }) {
+export default function Developer({ params }: { params: { developer:string } }) {
+    console.log("check developer name",params.developer)
+   const details=[
+    { id : 1, name: 'Yash', role: 'Senior Developer'},
+    { id : 2, name: 'Vaibhav', role: 'Backend Developer'},
+    { id : 3, name: 'Suresh', role: 'Frontend Developer'}
+    ]
+   const name=details.find(e=>e.id===Number(params.developer))
     return (
         <>
-            <h1>Name of the developer: {params.developer}</h1>
+            <h1>Name of the developer:{name?name.name:"enter a valid developer id"}</h1>
         </>
     )
 }
