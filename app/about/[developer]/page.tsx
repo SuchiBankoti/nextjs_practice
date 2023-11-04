028cc4c4bb1ad015d98c22f4991320af48fcf72d
suchi <suchibankoti95@gmail.com>
2023-11-04 13:19:31 +0530

    upadte about

--- app/about/[developer]/page.tsx
+++ app/about/[developer]/page.tsx
@@ -5,10 +5,11 @@ export default function Developer({ params }: { params: { developer:string } }) 
     { id : 2, name: 'Vaibhav', role: 'Backend Developer'},
     { id : 3, name: 'Suresh', role: 'Frontend Developer'}
     ]
-   const name=details.find(e=>e.id===Number(params.developer))
+   const person=details.find(e=>e.id===Number(params.developer))
     return (
         <>
-            <h1>Name of the developer:{name?name.name:"enter a valid developer id"}</h1>
+            <h1>Name of the developer:{person?person.name:"enter a valid developer id"}</h1>
+            {<h2>Designation:{person?person.role:"enter a valid developer id"}</h2>}
         </>
     )
 }
--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,26 @@
 import Link from "next/link"
 export default function About() {
+    const details=[
+        { id : 1, name: 'Yash', role: 'Senior Developer'},
+        { id : 2, name: 'Vaibhav', role: 'Backend Developer'},
+        { id : 3, name: 'Suresh', role: 'Frontend Developer'}
+        ]
     return (
         <>
             <h1>About Page</h1>
+            <div>
+                {
+                    details.map((e,i) => {
+                        return (
+                            <div key={i}>
+                                <Link href={`/about/${e.id}`}>
+                                {e.name}
+                                </Link>
+                            </div>
+                        )
+                    })
+                }
+            </div>
             <Link href="/">Link to home page</Link>
         </>
     )
